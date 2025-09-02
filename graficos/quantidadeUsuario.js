6fa4a5ad63d9f32beab65ae3390e88e85ff5d034
mariagabrielaaquino <maria.aquino.pinheiro@escola.pr.gov.br>
2025-09-02 08:14:58 -0300

    Update quantidadeUsuario.js

--- graficos/quantidadeUsuario.js
+++ graficos/quantidadeUsuario.js
@@ -1,24 +1,23 @@
-async function quantidadeUsuario() {
-    const url = "https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json"
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDosPostos = Object.keys(dados)
-const quantidadeTrabalhadores = Object.values(dados)
+async function quantidadeUsuarios() {
+        const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
+                const res = await fetch(url)
+                const dados = await res.json()
+                const nomeDosPostos = Object.keys(dados)
+            const quantidadeTrabalhadores = Object.values(dados)
+           
 
-
-const data = [
-    {
-        x: nomeDosPostos,
-        y:quantidadeTrabalhadores,
-        type: 'bar'
+                const data = [
+                {
+                x: nomeDosPostos,
+                y:quantidadeTrabalhadores,
+                type: 'bar'
     }
-]
+ ]
 
-const grafico = document.createElement('div')
-grafico.className = 'grafico'
-document.getElementById('grafico-container').appendChild(graficO)
-Plotly.newPlot(graficos, data)
-
+ const grafico = document.createElement('div')
+ grafico.className = 'grafico'
+ document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
 }
 
-quantidadeUsuario()
+quantidadeUsuarios()
